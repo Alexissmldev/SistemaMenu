e8f0336bb2e2685a8b61e9c85292b0ffdf14e24a
Alexis <alex.14mendoza14@gmail.com>
2025-11-11 11:25:12 -0400

    carrito de compras fase 2

--- js/jquery-3.7.1.js
+++ js/jquery-3.7.1.js
@@ -10,7 +10,7 @@
  */
 (function (global, factory) {
   "use strict";
-
+  
   if (typeof module === "object" && typeof module.exports === "object") {
     // For CommonJS and CommonJS-like environments where a proper `window`
     // is present, execute the factory and get jQuery.
